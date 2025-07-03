cef077ff308bfef8e6b3e0d251a5afe54850db70
Savita Sarode <sarodesavita64@gmail.com>
2025-07-04 00:34:34 +0530

    loop

---
+++ assign22.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+    int n,count;
+    printf("enter a number :");
+    scanf("%d", &n);
+
+    count = 0;
+    while(n!=0)
+    {
+        n/=10;
+        count++;
+    }
+    printf("total digit is :", count);
+
+    return 0;
+}
