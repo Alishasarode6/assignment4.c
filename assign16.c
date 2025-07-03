7a086098b7af9ceea3cc1425961030f216e4e46a
Savita Sarode <sarodesavita64@gmail.com>
2025-07-03 23:51:07 +0530

    loop

---
+++ assign16.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main(){
+    int i,sum=0;
+    printf("\n sumof even no from 100 to 200 :");
+
+    for(i=100;i<=200;i+=2)
+    sum+=i;
+
+    printf("\n sum is :%d", sum);
+
+    return 0;
+}
